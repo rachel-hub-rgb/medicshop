9c0a056cfb6a5d145e2351e79811b39fcfd445b5
rachel-hub-rgb <demonrank825@gmail.com>
2026-05-22 23:28:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let totalItems = 0;
 
+window.onload = function () {
+    loadItems();
+};
+
 function addItem() {
 
     let name = document.getElementById("itemName").value;
@@ -11,6 +15,21 @@ function addItem() {
         return;
     }
 
+    let item = {
+        name: name,
+        qty: qty
+    };
+
+    saveItem(item);
+
+    displayItem(item);
+
+    document.getElementById("itemName").value = "";
+    document.getElementById("itemQty").value = "";
+}
+
+function displayItem(item){
+
     let inventory = document.getElementById("inventoryList");
 
     let itemDiv = document.createElement("div");
@@ -19,7 +38,7 @@ function addItem() {
 
     let stockStatus = "";
 
-    if(qty <= 5){
+    if(item.qty <= 5){
         stockStatus = "Low Stock ⚠️";
         itemDiv.classList.add("low");
     }
@@ -29,11 +48,13 @@ function addItem() {
     }
 
     itemDiv.innerHTML = `
-        <h3>${name}</h3>
-        <p>Quantity: ${qty}</p>
+        <h3>${item.name}</h3>
+
+        <p>Quantity: ${item.qty}</p>
+
         <p>${stockStatus}</p>
 
-        <button onclick="deleteItem(this)">
+        <button onclick="deleteItem(this, '${item.name}')">
             Delete
         </button>
     `;
@@ -44,12 +65,34 @@ function addItem() {
 
     document.getElementById("totalItems")
     .innerText = totalItems;
-
-    document.getElementById("itemName").value = "";
-    document.getElementById("itemQty").value = "";
 }
 
-function deleteItem(button){
+function saveItem(item){
+
+    let items =
+    JSON.parse(localStorage.getItem("inventory"))
+    || [];
+
+    items.push(item);
+
+    localStorage.setItem(
+        "inventory",
+        JSON.stringify(items)
+    );
+}
+
+function loadItems(){
+
+    let items =
+    JSON.parse(localStorage.getItem("inventory"))
+    || [];
+
+    items.forEach(item => {
+        displayItem(item);
+    });
+}
+
+function deleteItem(button, itemName){
 
     button.parentElement.remove();
 
@@ -57,4 +100,17 @@ function deleteItem(button){
 
     document.getElementById("totalItems")
     .innerText = totalItems;
+
+    let items =
+    JSON.parse(localStorage.getItem("inventory"))
+    || [];
+
+    items = items.filter(
+        item => item.name !== itemName
+    );
+
+    localStorage.setItem(
+        "inventory",
+        JSON.stringify(items)
+    );
 }
